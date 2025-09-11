bf562cf6c2aeddefa669c64fd861eb2a43c7a597
Aryan <22bcs029@nith.ac.in>
2025-09-12 00:25:37 +0530

    fix: button link added

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { motion } from 'motion/react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Banner = () => {
+  const navigate = useNavigate()
   return (
     <motion.div 
     initial={{ opacity: 0, y: 50 }}
@@ -16,6 +18,9 @@ const Banner = () => {
             <p className='max-w-130'>We take care of insurance, driver verification and secure payments — so you can earn passive income, stress-free.</p>
 
             <motion.button 
+            onClick={() => {
+              navigate('/owner/add-car')
+            }}
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
             className='px-6 py-2 bg-white hover:bg-slate-100 transition-all text-primary rounded-lg text-sm mt-4 cursor-white-dot'>List your car</motion.button>
