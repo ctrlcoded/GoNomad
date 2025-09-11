acddcc80f0b4e5d748464b98690ea2b952ef4a20
Aryan <22bcs029@nith.ac.in>
2025-09-12 00:04:22 +0530

    fix: app provider fix provided

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import {AppProvider} from './context/AppContext.jsx'
-import {MotionConfig} from 'motion/react'
+import { StrictMode } from 'react'
+import { AppProvider } from './context/AppContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppProvider>
-      <MotionConfig viewport={{once: true}}>
+  <StrictMode>
+    <BrowserRouter>
+      <AppProvider>
         <App />
-      </MotionConfig>
-    </AppProvider>
-  </BrowserRouter>,
+      </AppProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
