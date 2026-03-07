e8b10b3b895f5086703d785b87c79a8454879f09
Aryan <22bcs029@nith.ac.in>
2026-03-07 22:30:16 +0530

    missing export default

--- server/server.js
+++ server/server.js
@@ -16,10 +16,13 @@ await connectDB()
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> res.send("Server is running"))
+app.get('/', (req, res) => res.send("Server is running"))
 app.use('/api/user', userRouter)
 app.use('/api/owner', ownerRouter)
 app.use('/api/bookings', bookingRouter)
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+// Export Express App for Vercel serverless functions
+export default app;
